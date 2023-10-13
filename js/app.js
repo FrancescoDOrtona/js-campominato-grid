37e42e81176b23d3764c868d2459b65c9631357e
FrancescoDOrtona <francescod.97@hotmail.it>
2023-10-13 16:42:29 +0200

    Add: JS function bg colour on click with for loop

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 // #Campo Minato
 
+// Funzioni
+function boxBgOnClick() {
+    this.classList.add('bg-azure');
+    console.log('La casella ha il numero:' + this.innerHTML)
+}
+
 // Variabili
 const gridDOMElement = document.querySelector('.grid');
 const playBtnDOMElement = document.querySelector('.play-btn');
@@ -7,7 +13,7 @@ const playBtnDOMElement = document.querySelector('.play-btn');
 //  Ciclo for per creare ed inserire gli elementi nel DOM
 playBtnDOMElement.addEventListener('click', function(){
 
-    gridDOMElement.innerHTML = ''
+    gridDOMElement.innerHTML = '';
 
     for (let i = 0; i < 100; i++){
         const n = (i + 1);
@@ -18,4 +24,16 @@ playBtnDOMElement.addEventListener('click', function(){
         gridDOMElement.innerHTML += boxDOMElement;  
     }
 
+    // Recupero le caselle dal DOM
+    const boxDOMElements = document.querySelectorAll('.box');
+    console.log(boxDOMElements);
+    
+    // Creo un ciclo per poi prendere ogni casella del DOM singolarmente
+    for (let i = 0; i < boxDOMElements.length; i++){
+
+        const currentBoxDOMElement = boxDOMElements[i]
+        console.log(currentBoxDOMElement)
+
+        currentBoxDOMElement.addEventListener('click', boxBgOnClick);       
+    }
 })
